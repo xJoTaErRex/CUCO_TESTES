f22cab83461847dc8ba83796600ac92ff3f1e2e2
xJoTaErRex <74562321+xJoTaErRex@users.noreply.github.com>
2022-05-23 02:03:35 -0300

    commit inicial

---
+++ CUCO_SELENIUM/src/main/java/br/ce/testes/TestesLogin.java
@@ -0,0 +1,51 @@
+package br.ce.testes;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestesLogin {
+
+	@Test
+    public void Login() {
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://localhost:4200");
+		System.out.println(driver.getTitle()); 
+		
+        //Escrever no campo informando seu id html
+		driver.findElement(By.xpath("/html/body/app-root/app-login/form/div/p[1]/input")).sendKeys("jr13freitas@hotmail.com");
+
+		driver.findElement(By.id("password")).sendKeys("12345678");
+		
+		driver.findElement(By.id("login-btn")).click();
+
+		//verificar a escrita no campo pelo seu id
+		Assert.assertEquals("Seja bem vindo, Junior Freitas!", driver.findElement(By.xpath("/html/body/app-root/app-top-menu/p-toolbar/div/div[2]")).getText());
+		
+		driver.quit();
+
+	}
+	
+	@Test
+    public void LoginInvalido() {
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://localhost:4200/login");
+		System.out.println(driver.getTitle()); 
+		
+        //Escrever no campo informando seu id html
+		driver.findElement(By.xpath("/html/body/app-root/app-login/form/div/p[1]/input")).sendKeys("jr13freitas@hotmail.com4");
+		
+		driver.findElement(By.id("password")).sendKeys("12345678855");
+		
+		driver.findElement(By.id("login-btn")).click();
+
+		//verificar a escrita no campo pelo seu id
+		Assert.assertEquals("Usuário e/ou senha incorretos!", driver.findElement(By.id("user-incorreto")).getText());
+		
+		driver.quit();
+
+	}
+	
+
+}
